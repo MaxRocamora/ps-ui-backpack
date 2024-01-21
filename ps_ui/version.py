7b14f03579b01a5dbfdfa2414092c69a716a5557
Maximilliano Rocamora <maxirocamora@gmail.com>
2024-01-21 15:03:27 -0300

    Update version.py with __version__ variable

--- ps_ui/version.py
+++ ps_ui/version.py
@@ -6,6 +6,6 @@ VERSION_MAJOR = 0
 VERSION_MINOR = 1
 VERSION_PATCH = 0
 
-version = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
+__version__ = f'{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}'
 
 app_name = 'pyside-ui-backpack'
