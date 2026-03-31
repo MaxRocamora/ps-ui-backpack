7beac777efbc7ed3a1ff5ff321367b8c60e431c4
Maximilliano Rocamora <maxirocamora@gmail.com>
2026-03-31 20:44:22 -0300

    Add dialog helper exports to __init__.py

---
+++ pyside_ui_backpack/dialogs/__init__.py
@@ -0,0 +1,15 @@
+"""Dialog helper exports."""
+
+from .dialogs import (
+    inform_dialog,
+    inform_dialog_small,
+    warning_dialog,
+    warning_dialog_small,
+)
+
+__all__ = [
+    'inform_dialog',
+    'inform_dialog_small',
+    'warning_dialog',
+    'warning_dialog_small',
+]
