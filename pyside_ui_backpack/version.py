6503b9cb6af92931084474ec1ee285bd4e472aa4
Maximilliano Rocamora <maxirocamora@gmail.com>
2026-03-31 20:49:57 -0300

    Update version comment to reflect Python support changes and future plans

--- pyside_ui_backpack/version.py
+++ pyside_ui_backpack/version.py
@@ -2,7 +2,8 @@
 # 0.1.0 01/2024 - Initial Test Release
 # 0.1.5 01/2024 - Added BG_COLORS to colors.py
 # 1.0.6 01/2025 - Added support for PySide6
-# 1.0.7 03/2026 - Added support for Python up to 3.15
+# 1.0.7 03/2026 - Python support updated to 3.9-3.15, docs/tests refreshed
+# Next planned change - Drop support for PySide2 and Python 3.9/3.10
 # ----------------------------------------------------------------------------------------
 
 VERSION_MAJOR = 1
